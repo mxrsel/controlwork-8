ae4d4f6bc1082d72141f11773f393cb0c813dc0c
mxrsel <marsel14082006@gmail.com>
2024-06-29 13:28:13 +0600

    Navbar was finish

--- src/containers/Navbar/Navbar.tsx
+++ src/containers/Navbar/Navbar.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <div className="logo">
+                <Link to="/">Quotes Central</Link>
+            </div>
+            <div className="categories">
+                <ul>
+                    <li><Link to="/quotes">All</Link></li>
+                    <li><Link to="/quotes/star-wars">Star wars</Link></li>
+                    <li><Link to="/quotes/famous-people">Famous People</Link></li>
+                    <li><Link to="/quotes/saying">Saying</Link></li>
+                    <li><Link to="/quotes/humour">Humor</Link></li>
+                    <li><Link to="/quotes/motivational">Motivational</Link></li>
+                </ul>
+            </div>
+            <div className="actions">
+                <Link to="/add">Submit new quote</Link>
+            </div>
+        </nav>
     );
 };
 
