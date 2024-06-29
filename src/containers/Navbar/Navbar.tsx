71d0e6195030a03c504d87d6cd42c2757514e58b
mxrsel <marsel14082006@gmail.com>
2024-06-29 13:30:59 +0600

    removed categories from navbar

--- src/containers/Navbar/Navbar.tsx
+++ src/containers/Navbar/Navbar.tsx
@@ -7,16 +7,6 @@ const Navbar: React.FC = () => {
             <div className="logo">
                 <Link to="/">Quotes Central</Link>
             </div>
-            <div className="categories">
-                <ul>
-                    <li><Link to="/quotes">All</Link></li>
-                    <li><Link to="/quotes/star-wars">Star wars</Link></li>
-                    <li><Link to="/quotes/famous-people">Famous People</Link></li>
-                    <li><Link to="/quotes/saying">Saying</Link></li>
-                    <li><Link to="/quotes/humour">Humor</Link></li>
-                    <li><Link to="/quotes/motivational">Motivational</Link></li>
-                </ul>
-            </div>
             <div className="actions">
                 <Link to="/add">Submit new quote</Link>
             </div>
